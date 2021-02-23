f95c8dd335b7ea0d8f872e875ad9e93ecf9b28f2
39nina <392mknn@gmail.com>
2021-02-23 18:05:44 +0900

    Add：Sceneを使用するアセットやoculus integrationを追加し、oculus quest2にビルドできる状態に設定

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Rigidbody rb;
+    float moveSpeed = 5;
+
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 SecondaryThumstick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+
+        // Y軸をZ軸に変換する
+        Vector3 velocity = new Vector3(SecondaryThumstick.x, 0, SecondaryThumstick.y);
+
+        rb.velocity = velocity * moveSpeed;
+    }
+}
