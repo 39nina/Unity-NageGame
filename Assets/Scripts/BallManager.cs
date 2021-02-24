a30a3d4f192247af926d44281d73bd1c116cbb4e
39nina <392mknn@gmail.com>
2021-02-24 22:31:54 +0900

    Add：各的の下に円柱のメッシュを追加し、接触判定のスクリプトを設定

---
+++ Assets/Scripts/BallManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallManager : MonoBehaviour
+{
+    float addPoint;
+    [SerializeField] UIManager UIManager;
+    bool isGrabbed = false;
+    bool isSleeping = false;
+
+    private void Update()
+    {
+        // 一度キューブを掴んだらisGrabbledをtrueに変更
+        if (this.GetComponent<Rigidbody>().IsSleeping() == false)
+        {
+            isGrabbed = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision target)
+    {
+        // 掴まれた後かつ、静止状態かつ、数字と接触していたら、ポイント表示に加算
+        if (isGrabbed == true && (target.gameObject.name == "10" || target.gameObject.name == "20" || target.gameObject.name == "30" || target.gameObject.name == "40" || target.gameObject.name == "50"))
+        {
+            addPoint = float.Parse(target.gameObject.name);
+            UIManager.AddPoint(addPoint);
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    float point = 0;
+    [SerializeField] Text pointText = default;
+
+    public void AddPoint(float addPoint)
+    {
+        point += addPoint;
+        pointText.text = point.ToString();
+    }
+}
